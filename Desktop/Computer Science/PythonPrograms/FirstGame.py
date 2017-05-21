0f3caecce777758686294bab8fb823081367f88d
Thomas Raney <raneytd@hendrix.edu>
2017-05-21 11:30:13 -0500

    Update FirstGame.py

--- Desktop/Computer Science/PythonPrograms/FirstGame.py
+++ Desktop/Computer Science/PythonPrograms/FirstGame.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.locals import *
 
 BLACK = (  0,   0,   0)
@@ -9,24 +9,132 @@ BLUE  = (  0,   0, 255)
 
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((400,300))
+DISPLAYSURF = pygame.display.set_mode((1280,800))
 pygame.display.set_caption('Snake: By Sanjit and Thomas')
 
-def new_dot():
-    pygame.draw.circle(gameDisplay, WHITE,\
-    (random.randint(50, 1230), random.randint(50, 750)), 3, 0)
+apple_position = ((0, 0))
+
+FPS = 1000000
+fpsClock = pygame.time.Clock()
+
+
+class Snake:
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.length = 1
+        self.position = ((self.x, self.y))
+
+    def draw_snake(self):
+        pygame.draw.circle(DISPLAYSURF, GREEN, self.position, 10, 0)
+
+    def change_position(self):
+        self.position = ((self.x, self.y))
+
+    def snake_delete(self):
+        pygame.draw.circle(DISPLAYSURF, BLACK, self.position, 10, 0)
+
+    def move_up(self):
+        self.snake_delete()
+        self.y -= 10
+        self.change_position()
+        self.draw_snake()
+
+    def move_down(self):
+        self.snake_delete()
+        self.y += 10
+        self.change_position()
+        self.draw_snake()
+
+    def move_right(self):
+        self.snake_delete()
+        self.x += 10
+        self.change_position()
+        self.draw_snake()
+
+    def move_left(self):
+        self.snake_delete()
+        self.x -= 10
+        self.change_position()
+        self.draw_snake()
+
+    def get_position(self):
+        return self.position
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+      
+def apple_delete():
+    
+    pygame.draw.circle(DISPLAYSURF, BLACK, apple_position, 5, 0)
+    
     pygame.display.update()
+    
 
+                 
+def new_apple():
+    
+    apple_delete()
+    
+    x_coordinate = random.randint(50, 1230)
+    while x_coordinate % 10 != 0:
+        x_coordinate = random.randint(50, 1230)
+    y_coordinate = random.randint(50, 750)
+    while y_coordinate % 10 != 0:
+        y_coordinate = random.randint(50, 750)
+    
+    pygame.draw.circle(DISPLAYSURF, WHITE,\
+    (x_coordinate, y_coordinate), 5, 0)
+    
+    apple_position = ((x_coordinate, y_coordinate))
 
-done = False
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    pygame.display.update()
+    
+    return apple_position
+     
+
+def game(snake):
+    snake_list = snake
+    snake_head = snake_list[0]
+    for snake in snake_list:
+        snake.draw_snake()
+    done = False
+    apple_position = new_apple()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_UP:
+                    for snake in snake_list:
+                        snake.move_up()
+                if event.key == K_DOWN:
+                    for snake in snake_list:
+                        snake.move_down()
+                if event.key == K_RIGHT:
+                    for snake in snake_list:
+                        snake.move_right()
+                if event.key == K_LEFT:
+                    for snake in snake_list:
+                        snake.move_left()
+            if snake_head.get_position() == apple_position:
+                x = snake_list[-1].get_x()
+                y = snake_list[-1].get_y()
+                snake_list.append(Snake(x - 10, y))
         pygame.display.update()
 
-quit()
+
+    quit()
+
+snake = [Snake(640, 400)]
+game(snake)
+
 
 
 
